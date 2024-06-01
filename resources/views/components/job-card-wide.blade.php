407b86d61b9d393fb028b1dbf91faf012647746e
Ушурбакиев Давлатбек Алимжанович <davlatbek.ushurbakiyev@ffin.kz>
2024-06-01 17:58:06 +0500

    finished implementing design layout of the project

---
+++ resources/views/components/job-card-wide.blade.php
@@ -0,0 +1,18 @@
+<x-panel class="flex gap-x-6">
+    <div>
+       <x-employer-logo />
+    </div>
+
+    <div class="flex-1 flex flex-col">
+        <a href="#" class="self-start text-sm">Laracasts</a>
+
+        <h3 class="font-bold text-xl mt-3 group-hover:text-blue-600 transition-colors duration-300">Video producer</h3>
+        <p class="text-sm text-gray-400 mt-auto">Full-time - from $60,000</p>
+    </div>
+
+    <div>
+        <x-tag>Back</x-tag>
+        <x-tag>Front</x-tag>
+        <x-tag>Devops</x-tag>
+    </div>
+</x-panel>
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,32 +5,34 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css2?family=Hanken+Grotesk:wght@400;500;600&display=swap"
+          rel="stylesheet">
     @vite(['resources/css/app.css','resources/js/app.js'])
     <title>Pixel position</title>
 </head>
-<body class="bg-black text-white">
-    <div class="px-10">
-        <nav class="flex justify-between items-center py-4 border-b border-white/10">
-            <a href="/">
-                <div>
-                    <img src="{{ Vite::asset('resources/images/logo.svg') }}" alt=""/>
-                </div>
-            </a>
-                <div class="space-x-6 font-bold">
-                    <a href="#">Jobs</a>
-                    <a href="#">Career</a>
-                    <a href="#">Salaries</a>
-                    <a href="#">Companies</a>
-                </div>
+<body class="bg-black text-white font-hanken-grotesk">
+<div class="px-10">
+    <nav class="flex justify-between items-center py-4 border-b border-white/10">
+        <a href="/">
+            <div>
+                <img src="{{ Vite::asset('resources/images/logo.svg') }}" alt=""/>
+            </div>
+        </a>
+        <div class="space-x-6 font-bold">
+            <a href="#">Jobs</a>
+            <a href="#">Career</a>
+            <a href="#">Salaries</a>
+            <a href="#">Companies</a>
+        </div>
 
-                <div>
-                    <a href="#">Post to a Job</a>
-                </div>
-        </nav>
+        <div>
+            <a href="#">Post to a Job</a>
+        </div>
+    </nav>
 
-        <main class="mt-10 max-w-[986px] mx-auto">
-            {{$slot}}
-        </main>
-    </div>
+    <main class="mt-10 max-w-[986px] mx-auto">
+        {{$slot}}
+    </main>
+</div>
 </body>
 </html>
